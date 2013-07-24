36e9d3c2ecbbf5d51cf6583d33f19a2c52f9b73b
Deviprasad Adiga <devi@dev.cloudmin.e2enetworks.net.in>
2013-07-24 11:00:00 +0530

    Changed Db connection to Nutrition reports

--- handlers/Nutrition.py
+++ handlers/Nutrition.py
@@ -7,8 +7,9 @@ import db.KLPDB
 import db.Queries
 from utils.CommonUtil import CommonUtil
 
-connection = db.KLPDB.getConnection()
-cursor = connection.cursor()
+#connection = db.KLPDB.getConnection()
+#cursor = connection.cursor()
+cursor = db.KLPDB.getWebDbConnection()
 
 class Nutrition:
 
@@ -27,7 +28,6 @@ class Nutrition:
       data.update(self.getMidDayMealData(constype,constid))
     except:
       print 'Error occurred....'
-      cursor.close()
       traceback.print_exc(file=sys.stderr)
     return data
 
@@ -35,31 +35,25 @@ class Nutrition:
     data = {}
     querykeys = ['dise_enrol','klp_enrol'] 
     for querykey in querykeys:
-      cursor.execute(db.Queries.getDictionary(constype)[constype + '_' + querykey],constid)
-      result = cursor.fetchall()
+      result = cursor.query(db.Queries.getDictionary(constype)[constype + '_' + querykey],{'s':constid})
       for row in result:
-        data[querykey] = {'numboys':row[0],'numgirls':row[1]} 
-      connection.commit()
+        data[querykey] = {'numboys':row.num_boys,'numgirls':row.num_girls} 
     querykey = 'sch_count'
-    cursor.execute(db.Queries.getDictionary(constype)[constype + '_' + querykey],constid)
-    result = cursor.fetchall()
+    result = cursor.query(db.Queries.getDictionary(constype)[constype + '_' + querykey],{'s':constid})
     for row in result:
-      data[querykey] = {'totsch':row[0],'mdmsch':row[1]} 
-    connection.commit()
+      data[querykey] = {'totsch':row.a1,'mdmsch':row.a2} 
     return data
 
   def getMidDayMealData(self,constype,constid):
     data = {}
     querykey = 'mdm_agg' 
-    cursor.execute(db.Queries.getDictionary(constype)[constype + '_' + querykey],constid)
-    result = cursor.fetchall()
+    result = cursor.query(db.Queries.getDictionary(constype)[constype + '_' + querykey],{'s':constid})
     tabledata ={}
     for row in result:
-      if row[0] in tabledata:
-        tabledata[row[0]][row[1]]=[row[2],row[3]]
+      if row.mon in tabledata:
+        tabledata[row.mon][row.wk]=[row.indent,row.attend]
       else:
-        tabledata[row[0]]={row[1]:[row[2],row[3]]}
-        data['const_name']=row[4]
+        tabledata[row.mon]={row.wk:[row.indent,row.attend]}
+        data['const_name']=row.name
     data[querykey] = tabledata
-    connection.commit()
     return data
